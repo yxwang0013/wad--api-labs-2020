6346244ca1786f24a63617e8664acd0c18d54bf2
yuxuan wang <2879296442@qq.com>
2020-12-27 17:53:43 +0000

    Week 9 Error HAndling

--- movies-api/index.js
+++ movies-api/index.js
@@ -6,14 +6,23 @@ import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
+};
 
 const port = process.env.PORT;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
-
+app.use(errHandler);
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
