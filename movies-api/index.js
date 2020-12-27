5f213a82eb7d557845b733da96866722b90fb051
yuxuan wang <2879296442@qq.com>
2020-12-27 00:46:16 +0000

    added routing for add,update,delete movie

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,12 +1,15 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
 app.use('/api/movies', moviesRouter);
