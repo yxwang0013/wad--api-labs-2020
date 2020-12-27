e7fcb2faa8f29982e262414c78e2e16389b1cb91
yuxuan wang <2879296442@qq.com>
2020-12-27 22:18:19 +0000

    Week 9 User Favourites

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -4,23 +4,48 @@ import User from './userModel';
 const router = express.Router(); // eslint-disable-line
 
 // Get all users
-router.get('/', (req, res,next) => {
-    User.find().then(users =>  res.status(200).json(users)).catch(next);
+router.get('/', (req, res, next) => {
+    User.find().then(users => res.status(200).json(users)).catch(next);
 });
 
 // register
-router.post('/', (req, res ,next) => {
-    User.create(req.body).then(user => res.status(200).json({success:true,token:"FakeTokenForNow"})).catch(next);
+router.post('/', (req, res, next) => {
+    User.create(req.body).then(user => res.status(200).json({ success: true, token: "FakeTokenForNow" })).catch(next);
 });
 
 // Update a user
-router.put('/:id',  (req, res,next) => {
+router.put('/:id', (req, res, next) => {
     if (req.body._id) delete req.body._id;
-     User.update({
-      _id: req.params.id,
+    User.update({
+        _id: req.params.id,
     }, req.body, {
-      upsert: false,
+        upsert: false,
     })
-    .then(user => res.json(200, user)).catch(next);
+        .then(user => res.json(200, user)).catch(next);
 });
+
+router.post('/:userName/favourites', (req, res, next) => {
+    const newFavourite = req.body;
+    const query = { username: req.params.userName };
+    if (newFavourite && newFavourite.id) {
+        User.find(query).then(
+            user => {
+                (user.favourites) ? user.favourites.push(newFavourite) : user.favourites = [newFavourite];
+                User.findOneAndUpdate(query, { favourites: user.favourites }, {
+                    new: true
+                }).then(user => res.status(201).send(user));
+            }
+        ).catch(next);
+    } else {
+        res.status(401).send("Unable to find user")
+    }
+});
+
+router.get('/:userName/favourites', (req, res, next) => {
+    const user = req.params.userName;
+    User.find({ username: user }).then(
+        user => res.status(201).send(user.favourites)
+    ).catch(next);
+});
+
 export default router;
