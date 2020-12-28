11328301f6bbd5f8c8b0b36381248c55722c706f
yuxuan wang <2879296442@qq.com>
2020-12-28 18:24:08 +0000

    Topic 10 - Exercises

--- movies-api/api/users/index.js
+++ movies-api/api/users/index.js
@@ -7,7 +7,7 @@ const router = express.Router(); // eslint-disable-line
 
 // Get all users
 router.get('/', (req, res, next) => {
-    User.find().then(users => res.status(200).json(users)).catch(next);
+  User.find().then(users => res.status(200).json(users)).catch(next);
 });
 
 // register
@@ -19,58 +19,78 @@ router.post('/', async (req, res, next) => {
     });
   }
   if (req.query.action === 'register') {
-    await User.create(req.body).catch(next);
-    res.status(201).json({
-      code: 201,
-      msg: 'Successful created new user.',
-    });
-  } else {
-    const user = await User.findByUserName(req.body.username).catch(next);
-      if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
-      user.comparePassword(req.body.password, (err, isMatch) => {
-        if (isMatch && !err) {
-          // if user is found and password is right create a token
-          const token = jwt.sign(user.username, process.env.SECRET);
-          // return the information including token as JSON
-          res.status(200).json({
-            success: true,
-            token: 'BEARER ' + token,
-          });
-        } else {
-          res.status(401).json({
-            code: 401,
-            msg: 'Authentication failed. Wrong password.'
-          });
-        }
+    const psd = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
+    if (psd.test(req.body.password)) {
+      await User.create(req.body).catch(next);
+      res.status(201).json({
+        code: 201,
+        msg: 'Successful created new user.',
+      });
+    } else {
+      res.status(401).json({
+        code: 401,
+        msg: 'Password is illegal.',
       });
     }
+  } else {
+    const user = await User.findByUserName(req.body.username).catch(next);
+    if (!user) return res.status(401).json({ code: 401, msg: 'Authentication failed. User not found.' });
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (isMatch && !err) {
+        // if user is found and password is right create a token
+        const token = jwt.sign(user.username, process.env.SECRET);
+        // return the information including token as JSON
+        res.status(200).json({
+          success: true,
+          token: 'BEARER ' + token,
+        });
+      } else {
+        res.status(401).json({
+          code: 401,
+          msg: 'Authentication failed. Wrong password.'
+        });
+      }
+    });
+  }
 });
 
 // Update a user
 router.put('/:id', (req, res, next) => {
-    if (req.body._id) delete req.body._id;
-    User.update({
-        _id: req.params.id,
-    }, req.body, {
-        upsert: false,
-    })
-        .then(user => res.json(200, user)).catch(next);
+  if (req.body._id) delete req.body._id;
+  User.update({
+    _id: req.params.id,
+  }, req.body, {
+    upsert: false,
+  })
+    .then(user => res.json(200, user)).catch(next);
 });
 
 router.post('/:userName/favourites', async (req, res, next) => {
-  const newFavourite = req.body.id;
-  const userName = req.params.userName;
-  const movie = await movieModel.findByMovieDBId(newFavourite);
-  const user = await User.findByUserName(userName);
-  await user.favourites.push(movie._id);
-  await user.save(); 
-  res.status(201).json(user); 
+  try {
+    const newFavourite = req.body.id;
+    const userName = req.params.userName;
+    const movie = await movieModel.findByMovieDBId(newFavourite);
+    const user = await User.findByUserName(userName);
+    if (user.favourites.indexOf(movie._id) !== -1) {
+      res.status(401).json({
+        code: 401,
+        msg: "Movie has already existed."
+      })
+    } else {
+      await user.favourites.push(movie._id);
+      await user.save();
+      res.status(201).json(user);
+    }
+  } catch (err) {
+    next(err)
+  }
 });
 router.get('/:userName/favourites', (req, res, next) => {
   const userName = req.params.userName;
   User.findByUserName(userName).populate('favourites').then(
     user => res.status(201).json(user.favourites)
   ).catch(next);
+
 });
 
 export default router;
